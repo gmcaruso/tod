66b19541a332bca2730e60fc3380c6f70d16304b
Gabriel Caruso <gabrielmcaruso@gmail.com>
2015-06-14 23:16:58 -0300

    Crear usuario -step, seteo el password en- verde

--- features/step_definitions/crear_un_usuario_steps.rb
+++ features/step_definitions/crear_un_usuario_steps.rb
@@ -6,8 +6,8 @@ Given(/^seteo el usuario en "(.*?)"$/) do |usuario|
   fill_in('user', :with => usuario)
 end
 
-Given(/^seteo el password en "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^seteo el password en "(.*?)"$/) do |password|
+  fill_in('password', :with => password)
 end
 
 Given(/^seteo el rol en "(.*?)"$/) do |arg1|
