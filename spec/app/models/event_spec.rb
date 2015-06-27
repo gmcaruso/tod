893db3e70927d8c173d162d38ff8fdd857074138
leoalonsoyvega <leoalonsoyvega@gmail.com>
2015-06-27 04:21:52 -0300

    agregar test_spec event.name olvidado

--- spec/app/models/event_spec.rb
+++ spec/app/models/event_spec.rb
@@ -3,6 +3,7 @@ require_relative '../../../app/models/event'
 describe Event do
   describe 'model' do
     it { should respond_to( :id) }
+    it { should respond_to( :name) }
     it { should respond_to( :amount_of_people) }
     it { should respond_to( :audience_level) }
     it { should respond_to( :max_amount_of_people) }
@@ -13,10 +14,12 @@ describe Event do
     it 'should create a event' do
 
       event = Event.new
+      event.name = 'Tarkovsky'
       event.amount_of_people = 20
       event.audience_level = 'Inicial'
       event.max_amount_of_people = 200
 
+      expect(event.name).to eq 'Tarkovsky'
       expect(event.amount_of_people).to eq 20
       expect(event.audience_level).to eq "Inicial"
       expect(event.max_amount_of_people).to eq 200
